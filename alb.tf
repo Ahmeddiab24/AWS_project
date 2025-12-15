7e103eced4aa5c161432f3a5535bff3da00b6874
ahmeddiab24 <ahmeddiab203@icloud.com>
2025-12-15 14:57:47 +0200

    creating alb with SG

---
+++ alb.tf
@@ -0,0 +1,44 @@
+resource "aws_alb" "app_lb" {
+  name               = "app-lb"
+  internal           = false # to be accessible from the internet not from inside vpc 
+  load_balancer_type = "application"
+  subnets            = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+  security_groups   = [aws_security_group.alb_sg.id]
+
+  tags = {
+    Name = "app_lb"
+  }
+}
+
+resource "aws_alb_target_group" "app_tg" {
+  name     = "app-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "app_tg"
+  }
+}
+
+resource "aws_alb_listener" "app_lb_listener" {
+  load_balancer_arn = aws_alb.app_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.app_tg.arn
+  }
+}
+
--- security_group.tf
+++ security_group.tf
@@ -1,3 +1,4 @@
+# Security group for jump server
 resource "aws_security_group" "jump_sg" {
   name        = "jump_sg"
   description = "Security group for jump server"
@@ -17,5 +18,27 @@ resource "aws_security_group" "jump_sg" {
     }
     tags = {
         Name = "jump_sg"
+    }
+}
+
+# Security group for ALB
+resource "aws_security_group" "alb_sg" {
+  name        = "alb_sg"
+  description = "Security group for ALB"
+  vpc_id      = aws_vpc.main.id 
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    tags = {
+        Name = "alb_sg"
     }
 }
